33571f9d2ab8dda1dd4b39d5d87200961bfee1ee
chaow porkaew <pchaowmobile@gmail.com>
2024-03-11 15:30:18 +0000

    update project

--- seapps/seapps_core/doctype/studentproject/studentproject.js
+++ seapps/seapps_core/doctype/studentproject/studentproject.js
@@ -1,8 +1,18 @@
 // Copyright (c) 2024, SE@UP and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("StudentProject", {
-// 	refresh(frm) {
+frappe.ui.form.on("StudentProject", {
+	refresh(frm) {
 
-// 	},
-// });
+		console.log(frm)
+
+		let user_filter = {
+			filters: {
+				"ignore_user_type": 1
+			}
+		}
+
+		frm.fields_dict['operators'].grid.get_field('operator').get_query = user_filter
+		frm.fields_dict['advisor'].grid.get_field('advisor').get_query = user_filter
+	},
+});
