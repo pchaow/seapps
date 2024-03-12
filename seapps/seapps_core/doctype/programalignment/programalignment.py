37239e1988e45c975e8ec3b3a447ba49b36b6862
chaow porkaew <pchaowmobile@gmail.com>
2024-03-12 11:05:31 +0000

    add alignment

--- seapps/seapps_core/doctype/programalignment/programalignment.py
+++ seapps/seapps_core/doctype/programalignment/programalignment.py
@@ -1,11 +1,11 @@
-# Copyright (c) 2024, SE@UP and contributors
+# Copyright (c) 2024, test and contributors
 # For license information, please see license.txt
-
+import frappe
 # import frappe
 from frappe.utils.nestedset import NestedSet
 
 
-class CourseGroup(NestedSet):
+class ProgramAlignment(NestedSet):
 	# begin: auto-generated types
 	# This code is auto-generated. Do not modify anything in this block.
 
@@ -14,13 +14,23 @@ class CourseGroup(NestedSet):
 	if TYPE_CHECKING:
 		from frappe.types import DF
 
-		course_type: DF.Data
+		display: DF.Data | None
 		is_group: DF.Check
+		key: DF.Data
 		lft: DF.Int
 		old_parent: DF.Link | None
-		parent_coursegroup: DF.Link | None
+		parent_programalignment: DF.Link | None
 		program: DF.Link | None
 		rgt: DF.Int
+		value: DF.Data
 	# end: auto-generated types
+
+	def before_save(self):
+		if self.parent_doc :
+			parent_program = frappe.get_doc("ProgramAlignment",self.parent_doc)
+			if parent_program :
+				self.parent = parent_program
+
+		self.display = f"{self.program} : {self.key} - {self.value}"
 
 	pass
